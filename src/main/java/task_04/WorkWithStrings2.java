c70ec2f04033f90c093d3d8bb8b218b92cec13d1
Fedor Fedichkin <f.fedichkin@gmail.com>
2017-07-07 19:41:26 +0300

    changed the way of output

--- src/main/java/task_04/WorkWithStrings2.java
+++ src/main/java/task_04/WorkWithStrings2.java
@@ -4,16 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class WorkWithStrings {
+public class WorkWithStrings2 {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             String s1 = reader.readLine();
-            char[] myCharArray = s1.toCharArray();
+            String s2 = "";
+            char[] myCharArray;
+            myCharArray = s1.toCharArray();
             for (int i = 0; i < myCharArray.length; i++) {
-                if (i % 2 == 0) {
-                    System.out.print(myCharArray[i] + " ");
+                if (i % 2 == 0){
+                    s2 = s2 + myCharArray[i];
                 }
             }
+            System.out.println(s2);
         } catch (IOException e) {
             e.printStackTrace();
         }
