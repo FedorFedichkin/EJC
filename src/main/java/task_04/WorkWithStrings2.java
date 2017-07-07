d3715a2d2226a9942ac2d1cb64c20a1565e83bde
Fedor Fedichkin <f.fedichkin@gmail.com>
2017-07-07 21:46:50 +0300

    Second class work task added

--- src/main/java/task_04/WorkWithStrings2.java
+++ src/main/java/task_04/WorkWithStrings2.java
@@ -8,15 +8,17 @@ public class WorkWithStrings2 {
     public static void main(String[] args) {
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             String s1 = reader.readLine();
-            String s2 = "";
-            char[] myCharArray;
-            myCharArray = s1.toCharArray();
-            for (int i = 0; i < myCharArray.length; i++) {
-                if (i % 2 == 0){
-                    s2 = s2 + myCharArray[i];
-                }
-            }
+            String s2 = s1;
+            s2 = s2.replaceAll(" ", "");
             System.out.println(s2);
+            StringBuilder s3 = new StringBuilder(s2);
+            StringBuilder s4 = s3.reverse();
+            s3 = new StringBuilder(s2);
+            if (s3.toString().equals(s4.toString())){
+                System.out.println(s1 + " is a polyndrom!");
+            } else {
+                System.out.println(s1 + " is not a polyndrom.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
