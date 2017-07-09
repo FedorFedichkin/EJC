e4cc07e9d6c43db27be20e2e2490b319a38ffd30
Fedor Fedichkin <f.fedichkin@gmail.com>
2017-07-10 01:52:29 +0300

    Palindrome misspelling fixed

--- src/main/java/task_04/WorkWithStrings2.java
+++ src/main/java/task_04/WorkWithStrings2.java
@@ -4,11 +4,11 @@ public class WorkWithStrings2 {
     public static void main(String[] args) {
         String[] strings = new String[] {"АРГЕНТИНА МАНИТ НЕГРА", "ПОТ КАК ПОТОП", "А РОЗА УПАЛА НА ЛАПУ АЗОРА"};
         for (String string : strings) {
-            System.out.println(isPolyndrom(string) ? string + " is a polyndrom!" : string + " is not a polyndrom.");
+            System.out.println(isPalindrome(string) ? string + " is a palindrome!" : string + " is not a palindrome.");
         }
     }
 
-    private static boolean isPolyndrom(String s){
+    private static boolean isPalindrome(String s){
         String string = s.replaceAll(" ", "");
         StringBuilder stringBuilder = new StringBuilder(string).reverse();
         return (string.equals(stringBuilder.toString()));
