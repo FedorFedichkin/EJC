254704766a5ba557b8bb61166c9299ccc20aca64
Fedor Fedichkin <f.fedichkin@gmail.com>
2017-07-10 00:55:59 +0300

    Second task on String is rewritten in more compact way

--- src/main/java/task_04/WorkWithStrings2.java
+++ src/main/java/task_04/WorkWithStrings2.java
@@ -4,19 +4,13 @@ public class WorkWithStrings2 {
     public static void main(String[] args) {
         String[] strings = new String[] {"АРГЕНТИНА МАНИТ НЕГРА", "ПОТ КАК ПОТОП", "А РОЗА УПАЛА НА ЛАПУ АЗОРА"};
         for (String string : strings) {
-            if (isPolyndrom(string)) {
-                System.out.println(string + " is a polyndrom!");
-            } else {
-                System.out.println(string + " is not a polyndrom.");
-            }
+            System.out.println(isPolyndrom(string) ? string + " is a polyndrom!" : string + " is not a polyndrom.");
         }
     }
 
-    private static boolean isPolyndrom(String string){
-        String s = string.replaceAll(" ", "");
-        StringBuilder s1 = new StringBuilder(s);
-        StringBuilder s2 = s1.reverse();
-        s1 = new StringBuilder(s);
-        return (s1.toString().equals(s2.toString()));
+    private static boolean isPolyndrom(String s){
+        String string = s.replaceAll(" ", "");
+        StringBuilder stringBuilder = new StringBuilder(string).reverse();
+        return (string.equals(stringBuilder.toString()));
     }
 }
