d69bbf2aed265c5a64023de6582f07a2b326602a
Fedor Fedichkin <f.fedichkin@gmail.com>
2017-07-27 05:59:43 +0300

    Merge sort and Bubble sort are added. Binary search is in MergeSortRealistion class.

---
+++ src/main/java/task_08_sort2/BubbleSortRealisation.java
@@ -0,0 +1,62 @@
+package task_08_sort2;
+
+import java.util.Random;
+
+/**
+ * This class was made to demonstrate classic bubble sort in an array of 100
+ * elements with values generated randomly from 1 to 1000.
+ * @author Fedor Fedichkin
+ * */
+public class BubbleSortRealisation {
+    private static int[] arrayToSort = new int[100];
+
+    public static void main(String[] args) {
+        BubbleSortRealisation bubbleSortRealisation = new BubbleSortRealisation();
+        Random random = new Random();
+        for (int i = 0; i < arrayToSort.length; i++) {
+            arrayToSort[i] = random.nextInt(999)+1;
+        }
+        System.out.println("Unsorted array:");
+        bubbleSortRealisation.arrayToConsole(arrayToSort);
+        bubbleSortRealisation.bubbleSort(arrayToSort);
+        System.out.println();
+        System.out.println("Sorted array:");
+        bubbleSortRealisation.arrayToConsole(arrayToSort);
+    }
+
+    /**
+     * Method where bubble sorting happens
+     * @param arrayToSort - array to sort
+     * */
+    private void bubbleSort(int[] arrayToSort){
+        for (int i = 0; i < arrayToSort.length; i++) {
+            for (int j = i + 1; j < arrayToSort.length; j++) {
+                if (arrayToSort[i] > arrayToSort[j]){
+                    swap(i, j);
+                }
+            }
+        }
+    }
+
+    /**
+     * Method that prints array to console
+     * @param arrayToConsole - array to print
+     * */
+    private void arrayToConsole(int[] arrayToConsole){
+        for (int anArrayToConsole : arrayToConsole) {
+            System.out.print(anArrayToConsole + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Kind of sophisticated swap function for two elements in array at positions i and j.
+     * @param i - array position number of the first element to swap
+     * @param j - array position number of the second element to swap
+     * */
+    private static void swap(int i, int j){
+        arrayToSort[i] = arrayToSort[i] ^ arrayToSort[j];
+        arrayToSort[j] = arrayToSort[i] ^ arrayToSort[j];
+        arrayToSort[i] = arrayToSort[i] ^ arrayToSort[j];
+    }
+}
---
+++ src/main/java/task_08_sort2/MergeSortRealisation.java
@@ -0,0 +1,93 @@
+package task_08_sort2;
+
+import java.util.Random;
+
+/**
+ * This class was made to demonstrate classic bubble sort in an array of 100
+ * elements with values generated randomly from 1 to 1000.
+ * @author Fedor Fedichkin
+ * */
+public class MergeSortRealisation {
+    private static int[] arrayToSort = new int[100];
+
+    public static void main(String[] args) {
+        MergeSortRealisation mergeSortRealisation = new MergeSortRealisation();
+        Random random = new Random();
+        for (int i = 0; i < arrayToSort.length - 1; i++) {
+            arrayToSort[i] = random.nextInt(999)+1;
+        }
+        int myValue = 123;
+        arrayToSort[arrayToSort.length - 1] = myValue;
+        System.out.println("Unsorted array:");
+        mergeSortRealisation.arrayToConsole(arrayToSort);
+        mergeSortRealisation.mergeSort(arrayToSort, 0, arrayToSort.length);
+        System.out.println();
+        System.out.println("Sorted array:");
+        mergeSortRealisation.arrayToConsole(arrayToSort);
+        int indexOfMyValueInSortedArray = mergeSortRealisation.binarySearchInArray(myValue);
+        System.out.println("Index of my value " + myValue + " in sorted array is: " + indexOfMyValueInSortedArray);
+    }
+
+    /**
+     * Method where merge sorting happens
+     * @param arrayToSort - array to sort
+     * @param left - left border of array
+     * @param right - right border of array
+     * */
+    private void mergeSort(int[] arrayToSort, int left, int right){
+        if (left + 1 < right){
+            int middle = (left + right) / 2;
+            mergeSort(arrayToSort, left, middle);
+            mergeSort(arrayToSort, middle, right);
+
+            int length = right - left;
+            int[] arrayBuffer = new int[length];
+            int currentLeft = left;
+            int currentMiddle = middle;
+            for (int i = 0; i < length; i++) {
+                if (currentMiddle >= right ||
+                        currentLeft < middle &&
+                                arrayToSort[currentLeft] < arrayToSort[currentMiddle]){
+                    arrayBuffer[i] = arrayToSort[currentLeft++];
+                } else {
+                    arrayBuffer[i] = arrayToSort[currentMiddle++];
+                }
+            }
+            System.arraycopy(arrayBuffer, 0, arrayToSort, left, length);
+        }
+    }
+
+    /**
+    * Binary search of an index of a certain value in sorted array
+    * @param value - an element to find
+    * @return - index of searched value in array (starts with 0)
+    * */
+    private int binarySearchInArray(int value){
+        int left = 0;
+        int right = arrayToSort.length - 1;
+        int middle;
+        while (true)
+        {
+            middle = left + (right - left) / 2;
+            if (arrayToSort[middle] == value){
+                return middle;
+            }
+            if (arrayToSort[middle] > value){
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+    }
+
+    /**
+     * Method that prints array to console
+     * @param arrayToConsole - array to print
+     * */
+    private void arrayToConsole(int[] arrayToConsole){
+        for (int anArrayToConsole : arrayToConsole) {
+            System.out.print(anArrayToConsole + " ");
+        }
+        System.out.println();
+    }
+}
